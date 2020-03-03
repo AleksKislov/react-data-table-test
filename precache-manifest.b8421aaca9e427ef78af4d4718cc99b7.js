781b191228216dda33eea34c17821230241fae69
AleksKislov <sinokislov@gmail.com>
2020-03-03 13:32:12 +0300

    Updates

---
+++ precache-manifest.b8421aaca9e427ef78af4d4718cc99b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a1b366d69d8302be7bdb6c4e758644e",
+    "url": "/react-data-table-test/index.html"
+  },
+  {
+    "revision": "e5e4e39ca48856fde637",
+    "url": "/react-data-table-test/static/css/main.36d5c65e.chunk.css"
+  },
+  {
+    "revision": "b40526e1103ef4ab5b26",
+    "url": "/react-data-table-test/static/js/2.63b3703a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-data-table-test/static/js/2.63b3703a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5e4e39ca48856fde637",
+    "url": "/react-data-table-test/static/js/main.86e65e22.chunk.js"
+  },
+  {
+    "revision": "3e50e4cafd3cebedc484",
+    "url": "/react-data-table-test/static/js/runtime-main.9a2e38fe.js"
+  }
+]);
